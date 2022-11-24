9dd25e8363aca494776161d7c76d131efe829b18
shrawankansi <shrawankansi@gmail.com>
2022-11-24 22:00:20 +0530

    add

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -27,7 +27,7 @@ const Header = () => {
                         <li><a href="features">features</a></li>
                         <li><a href="portfolio">portfolio</a></li>
                         <li><a href="resume">resume</a></li>
-                        <li><a href="clients">clients</a></li>
+                        {/* <li><a href="clients">clients</a></li> */}
                         <li><a href="blog">blog</a></li>
                         <li><a href="contact">contact</a></li>
                         <li> <DayNightToggle
