9d12d8f20ac7752302974959044664004988bb2d
shrawankansi <shrawankansi@gmail.com>
2022-12-02 09:33:20 +0530

    update

--- src/Component/Portfolio/Portfolio_data.js
+++ src/Component/Portfolio/Portfolio_data.js
@@ -1,50 +1,51 @@
+
 const Portfolio_data = [
     {
       id: 1,
       category: "Web Development",
       totalLike: "150",
       title: "Shopify Website clone using TailwindCSS",
-      image: "https://drive.google.com/file/d/1sDP7SKygzaypnVIeBFArbsvlrs5Cf7vW/view?usp=share_link",
+      image: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEieZPt2ojgPAGkiKke3fJGiSuJEwmXRKT-vQl18RDHZds1y03eEnrakV3aW47K6thADrpzQGKaiofBl6GKQnuuRXgwKupPyjEWMlPTOPRPcUi7UDI2QfQnx-8dNxBiHZEQzGh3SCXtkx4nDX_HslzhJv7MfhPhcFCdiS10X5-E4H3a0NUi21Hapw7mY/s320/shopify-clone-404.png" ,
       
     },
     {
       id: 2,
-      category: "APPLICATION",
+      category: "Web Development",
       totalLike: "750",
-      title: "Mobile app landing design & maintain",
-      image: "https://drive.google.com/file/d/1ds5F1z6V-vNwGoWh7SD8SEg_KkaN5FpW/view?usp=sharing",
+      title: "Rode.com Website clone using TailwindCSS",
+      image: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjricGUAZVzlaKGo3o0C4MGE6Ur-lJTXnXXNWi0Xz-WyYoktrt9YsYSpsLrX04JrPqHrqJREP75Srpw96XnU_Qq0_m87XvOfWsJdKfip0n8qlj8fJlOOfQHEZ-8D2mLZEtqUl9VIgRwBkn35d-7RXNX2tHBGfa_YuTuQfjJT1o8sajikqExAdfUW6bj/s320/rode-clone-40.png',
 
     },
     {
       id: 3,
-      category: "PHOTOSHOP",
+      category: "Web Development",
       totalLike: "630",
-      title: "Logo design creativity & Application ",
-      image: "https://drive.google.com/file/d/1sebk-oBX__Q_1pRoT5fztTGq8Npx4Rti/view?usp=share_link",
+      title: "Paytm Website clone using TailwindCSS",
+      image: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgwoWnEXSOWvB1Z-yKdmyskfi3Jsovkg31MgiveKJ4fIorroCJImR55Uixl0c-FX0IwGhh1sPiaVgmwRR3DUUqYQZWozoRR1OA4eHnX4GkYY-Aoi2ZfdX6mQNXZkc2DI4r8l9s9vp0deWvL6yP-IZKTRQ_fIPwx5RDfEJqSdR528YDQrKsCqiu3HTWq/s320/paytm-clone-404.png",
       
     },
     {
       id: 4,
-      category: "FIGMA",
+      category: "Web Development",
       totalLike: "360",
-      title: "Mobile app landing design & Services",
-      image: "https://github.com/shrawankansi/project-15/blob/main/project-15.png?raw=true",
+      title: " landing design & Services",
+      image: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhI3yLTAO5sgjcDk16vATeSpmCnuYqrYgHuDYxaTJWEiBx-RV6kWM8rKU5j5lIoeR21YEspBDqde7EZYH1CuVwh6saztG7wKM2DW86drFVpK5-_fh5p-bEzyZiZuVmaxEj-n0hEyMhkPjDZDTd0VAe93QOqSw-k9MGb6e8rJoLxcDJtnJcRN_7qyUah/s320/myineuron-404.png",
       
     },
     {
       id: 5,
-      category: "WEB DESIGN",
+      category: "Web Development",
       totalLike: "280",
-      title: "Design for tecnology & services",
-      image: "https://github.com/shrawankansi/hackathon/blob/main/myineuron-404.png?raw=true",
+      title: "Design for Portfolio",
+      image: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhAtWhWaq8ML34KqojIVrztfbG_yg4v2ay8wSwdtWqt169NXuHQXGUlecJvOoq3dwEoQNeIfovvANYjcUsQbsqdq9BNRQXluufe2vGhvzW2l1ycvfEWO7eP9TkTOMRWrcz-Nj8qQEwttAE5a0dIw6CvwtSRNNqSvtfh18Fo1GGACNpHS9nPbRz10mdj/s320/project-15.png",
       
     },
     {
       id: 6,
-      category: "WEB DESIGN",
+      category: "Web Development",
       totalLike: "690",
       title: "App for tecnology & services",
-      image: "https://github.com/shrawankansi/project-9/blob/main/project-9.png?raw=true",
+      image: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi5Jw4JwPvH0uzx6eyw9aNoLTKdaXqNFe8Wx9zxil32FEFSudUI0_TlSQ8Q7QelO4Si0uDwIcZhtMJvSPrdwBGT2-oa9rRJzpEFPRvFm26SE4Pb5UZ3s7dlOyQv60CrhJbBxxcvdAIujJiTRZLITmmSQ28GCqOgm2D1yuIU9uKWgrJbM55ObVpyd0_8/s320/project-9.png",
       
     },
   ]
